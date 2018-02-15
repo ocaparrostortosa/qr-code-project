ed6a8de8b5c02fefa09350821851d08c4392b692
ocaparrostortosa <ocaparrostortosa@gmail.com>
2018-02-15 17:58:35 +0100

    First commit

---
+++ app/src/main/java/com/vapeplanner/qrcodeproyect/MainQrCodeActivity.java
@@ -0,0 +1,13 @@
+package com.vapeplanner.qrcodeproyect;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainQrCodeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_qr_code);
+    }
+}
---
+++ app/src/main/java/com/vapeplanner/qrcodeproyect/QrCodeReader.java
@@ -0,0 +1,13 @@
+package com.vapeplanner.qrcodeproyect;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class QrCodeReader extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qr_code_reader);
+    }
+}
